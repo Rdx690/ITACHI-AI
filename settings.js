f792a2cc2df57986ebe332b54007ab2fda4b784d
Rdx690 <sourajitsarkar59@gmail.com>
2025-09-11 09:59:43 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'ITACHI-AI•SOURAJIT',
+  author: '‎',
+  botName: "ITACHI-AI",
+  botOwner: 'SOURAJIT', // Your name
+  ownerNumber: '916909950582', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
